c455a6ec16ed9a06ef2daf340109d963434e1611
Jose Borreguero <borreguero@gmail.com>
2018-01-11 11:28:01 -0500

    Refs #29 use multiprocessing

--- idpflex/distances.py
+++ idpflex/distances.py
@@ -1,5 +1,8 @@
 from __future__ import print_function, absolute_import
 
+import random
+from functools import partial
+import multiprocessing
 import numpy as np
 from scipy.spatial.distance import squareform
 from MDAnalysis.analysis.rms import rmsd as find_rmsd
@@ -31,6 +34,33 @@ def extract_coordinates(a_universe, group, indexes=None):
     return np.asarray(xyz)
 
 
+def rmsd_rows(i_chunk, coords):
+    r"""RMDS values for a set of row indexes
+
+    Parameters
+    ----------
+    i_chunk : :py:class:`list`
+        list of row indexes
+    coords : :class:`~numpy:numpy.ndarray`
+        Atomic coordinates
+
+    Returns
+    -------
+    i_chunk : :py:class:`list`
+        Input list of row indexes
+    rmsd_chunk : :py:class:`list`
+        list of lists where each list item is a list of RMSD values
+    """
+    n = len(coords)
+    rmsd_chunk = list()
+    for i in i_chunk:
+        ri = coords[i]
+        rmsd_values = [find_rmsd(ri, coords[j], superposition=True)
+                       for j in range(i+1, n)]
+        rmsd_chunk.append(rmsd_values)
+    return i_chunk, rmsd_chunk
+
+
 def rmsd_matrix(xyz, condensed=False):
     r"""RMSD matrix between coordinate frames.
 
@@ -48,12 +78,24 @@ def rmsd_matrix(xyz, condensed=False):
     """
     n = len(xyz)
     rmsd = np.zeros(n * n).reshape(n, n)
-    # To-Do: make parallel
-    for i in range(0, n-1):
-        ri = xyz[i]
-        for j in range(i+1, n):
-            rmsd[i][j] = find_rmsd(ri, xyz[j], superposition=True)
-            rmsd[j][i] = rmsd[i][j]
+    # RMSD is a symmetric matrix with zeros in the diagonal. Thus, we only
+    # calculate the upper diagonal.
+    # Divide all rows of RMSD among available cores
+    indexes = list(range(0, n-1))  # row indexes
+    random.shuffle(indexes)  # to balance load among cores
+    n_cores = multiprocessing.cpu_count()
+    m = int(n / n_cores)  # number of rows per core
+    # Each chunk of rows is assigned to one core
+    i_chunks = [indexes[i*m: (i+1)*m] for i in range(n_cores)]
+    # left over rows assigned to last chunk
+    i_chunks[-1].extend(indexes[m * n_cores:])
+
+    with multiprocessing.Pool(processes=n_cores) as pool:
+        for i_chunk, dists in pool.imap_unordered(partial(rmsd_rows, coords=xyz), i_chunks):
+            for k, i in enumerate(i_chunk):
+                rmsd[i][i+1:] = dists[k]
+    rmsd += rmsd.transpose()
+
     if condensed is True:
         rmsd = squareform(rmsd)
     return rmsd
