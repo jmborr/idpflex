27587f21f3b66e082230558cbd99b71c37765274
Jose Borreguero <borreguero@gmail.com>
2018-02-09 16:30:00 -0500

    Refs #47 release 0.1.4

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='idpflex',
-    version='0.1.3',
+    version='0.1.4',
     long_description=readme,
     author="Jose Borreguero",
     author_email='borreguero@gmail.com',
