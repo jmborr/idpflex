06a6031b15f07206251ecd0f938e28997c446364
Jose Borreguero <borreguero@gmail.com>
2018-12-21 14:07:18 -0500

    Refs #85 source for the decorator

--- idpflex/utils.py
+++ idpflex/utils.py
@@ -1,6 +1,9 @@
 import os
 from contextlib import contextmanager
 import tempfile
+import functools
+from collections import namedtuple, Mapping
+
 import MDAnalysis as mda
 
 
@@ -46,3 +49,32 @@ def temporary_file(**kwargs):
         yield name
     finally:
         os.remove(name)
+
+
+def namedtuplefy(func):
+    r"""
+    Decorator to transform the return dictionary of a function into
+    a namedtuple
+
+    Parameters
+    ----------
+    func: Function
+        Function to be decorated
+    name: str
+        Class name for the namedtuple. If None, the name of the function
+        will be used
+    Returns
+    -------
+    Function
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        if wrapper.nt is None:
+            if isinstance(res, Mapping) is False:
+                raise ValueError('Cannot namedtuplefy a non-dict')
+            wrapper.nt = namedtuple(func.__name__ + '_nt', res.keys())
+        return wrapper.nt(**res)
+    wrapper.nt = None
+    return wrapper
