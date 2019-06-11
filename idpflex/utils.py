b8da45603e22414999dab0298fa0d66418759f94
Connor Pigg <cpigg2@illinois.edu>
2019-06-11 18:16:02 -0400

    Closes #92 by calculating trajectories for entire profiles.

--- idpflex/utils.py
+++ idpflex/utils.py
@@ -2,33 +2,34 @@ import os
 from contextlib import contextmanager
 import tempfile
 import functools
+import multiprocessing
 from collections import namedtuple, Mapping
 
 import MDAnalysis as mda
 
 
-def write_frame(a_universe, iframe, file_name):
+def write_frame(atom_group, iframe, file_name):
     r"""Write a single trajectory frame to file.
 
     Format is guessed from the file's extension.
 
     Parameters
     ----------
-    a_universe : :class:`~MDAnalysis.core.universe.Universe`
-        Universe describing the simulation
+    atom_group : :class:`~MDAnalysis.AtomGroup`
+        Atoms from the universe describing the simulation
     iframe : int
         Trajectory frame index (indexes begin with zero)
     file_name : str
         Name of the file to create
     """
-    a_universe.trajectory[iframe]
+    atom_group.universe.trajectory[iframe]
     # Create directory if not existing
     dir_name = os.path.dirname(file_name)
     if dir_name and not os.path.isdir(dir_name):
         os.makedirs(dir_name)
 
     with mda.Writer(file_name) as writer:
-        writer.write(a_universe)
+        writer.write(atom_group)
 
 
 @contextmanager
@@ -58,7 +59,7 @@ def namedtuplefy(func):
 
     Parameters
     ----------
-    func: Function
+    func: function
         Function to be decorated
     name: str
         Class name for the namedtuple. If None, the name of the function
@@ -78,3 +79,52 @@ def namedtuplefy(func):
         return wrapper.nt(**res)
     wrapper.nt = None
     return wrapper
+
+
+def generate_profile_for_frame(atom_group, iframe, profile_class):
+    r"""
+    Utility function to generate profile properties for a frame in a trajectory.
+
+    Parameters
+    ----------
+    atom_group: :class:`MDAnalysis.AtomGroup`
+        The atom group representing the structure to calculate the profile for.
+    iframe: int
+        The index of a trajectory for which to calculate profiles of the associated atom_group.
+    profile_class:
+        The profile class to use for the properties to be returned and calculated. Must implement a `from_pdb` method.
+
+    Returns
+    -------
+    Profile for the selected frame
+    """  # noqa: E501
+    with temporary_file(suffix='.pdb') as fname:
+        # Copy the atom group to a new universe to avoid
+        # changing frames upon writing
+        u = atom_group.universe
+        u2 = mda.Universe(u.filename, u.trajectory.filename)
+        atoms2 = u2.atoms[atom_group.atoms.indices]
+        write_frame(atoms2, iframe, fname)
+        return profile_class().from_pdb(fname)
+
+
+def generate_trajectory_profiles(atom_group, iframes, profile_class):
+    r"""
+    Utility function to generate profile properties for each frame in a trajectory.
+
+    Parameters
+    ----------
+    atom_group: :class:`MDAnalysis.AtomGroup`
+        The atom group representing the structure to calculate the profile for.
+    iframes: List[int]
+        The indices of a trajectory for which to calculate profiles of the associated atom_group.
+    profile_class:
+        The profile class to use for the properties to be returned and calculated. Must implement a `from_pdb` method.
+
+    Returns
+    -------
+    List of the profiles.
+    """  # noqa: E501
+    with multiprocessing.Pool() as p:
+        return p.starmap(generate_profile_for_frame,
+                         [(atom_group, i, profile_class) for i in iframes])
